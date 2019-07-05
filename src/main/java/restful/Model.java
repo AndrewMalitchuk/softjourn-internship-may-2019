9e76d698eed6dc54985ddd3ee321870da818aa58
Andrew <andrew@mail.com>
2019-07-05 13:17:57 +0300

    Create base for the RESTful

---
+++ src/main/java/restful/Model.java
@@ -0,0 +1,18 @@
+package restful;
+
+/***
+ * This model class was created 'as is' - just for testing correct of works of RESTful
+ */
+public class Model {
+
+    private final String content;
+
+    public Model(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
