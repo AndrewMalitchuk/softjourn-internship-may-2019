5d1e83722fbfcbc426b6b59836a5c4284cbe3300
Andrew <andrew@mail.com>
2019-07-05 18:38:13 +0300

    Try Spring Security

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,4 +1,4 @@
-package restful;
+package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/restful/ModelController.java
+++ src/main/java/restful/ModelController.java
@@ -7,18 +7,18 @@ import org.springframework.web.bind.annotation.RestController;
 /***
  * This class-controller was created 'as is' - just for testing correct of works of RESTful
  */
-@RestController
-public class ModelController {
-
-
-    @RequestMapping("/about")
-    public Model about(@RequestParam(value = "content", defaultValue = "OK") String content) {
-        return new Model(content);
-    }
-
-    @RequestMapping("/")
-    public String root(){
-        return "Everything works fine.";
-    }
-
-}
+//@RestController
+//public class ModelController {
+//
+//
+//    @RequestMapping("/about")
+//    public Model about(@RequestParam(value = "content", defaultValue = "OK") String content) {
+//        return new Model(content);
+//    }
+//
+//    @RequestMapping("/")
+//    public String root(){
+//        return "Everything works fine.";
+//    }
+//
+//}
